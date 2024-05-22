b4dfaab66a08961103afd15b76900b50eb1d5de6
victor@victor <victor.dmaina@gmail.com>
2024-05-22 03:26:02 +0300

    pass conditional statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -4,14 +4,53 @@ def admin_login(username, password):
     # your code here
     pass
 
+def admin_login(username, password):
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+    
 def hows_the_weather(temperature):
     # your code here
     pass
 
+def hows_the_weather(temperature):
+    if (temperature < 40 ):
+        return "It's brisk out there!"
+    elif(temperature > 40 and temperature < 65 ):
+        return "It's a little chilly out there!"
+    elif(temperature > 85):
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+    
 def fizzbuzz(num):
     # your code here
     pass
 
+def fizzbuzz(num):
+    if (num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    elif (num % 3 == 0):
+        return "Fizz"
+    elif (num % 5 == 0):
+        return "Buzz"
+    else:
+        return num
+
 def calculator(operation, num1, num2):
     # your code here
     pass
+
+def calculator(operation, num1, num2):
+    if operation not in ('+', '-', '*', '/'):
+        print("Invalid operation!")
+        return None
+    if (operation == "+"):
+        return num1 + num2
+    elif (operation == "-"):
+        return num1 - num2
+    elif (operation == "*"):
+        return num1 * num2
+    elif (operation == "/"):
+        return num1 / num2
